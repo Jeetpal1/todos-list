c9d99b4c70619adabc96ef364eb777e5556344d9
Jeetpal <singhjeetpal99@gmail.com>
2022-05-02 18:52:33 -0500

    changed padding of about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const About = () => {
   return (
-    <div>
+    <div className="about-info">
       This is an about function.
       <p>
         Jeetpal's Todolist is a react based project. I created this to revise my web development skills. Moreover, I use todo list in my daily routine, that is I wanted to created my own todo list app, in which I can customize things according to me. This app uses local storage to store the add tasks.
